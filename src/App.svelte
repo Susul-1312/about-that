990a317a9d7c5d30f7e6ae766d77ea9c6ff4d012
Susul-1312 <susl@rickroll.link>
2022-05-18 18:08:05 +0200

    Create working prototype of card system

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Card from './lib/Card.svelte';
+</script>
+
+<Card  title="Test Card">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi unde distinctio excepturi id illum nam dolorem quidem iure, laudantium labore pariatur beatae praesentium nisi quos expedita repellat molestias minima. Aliquam.
+</Card>
---
+++ src/lib/Card.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let title;
+    export let imageURL = null;
+    export let imageAlt = null;
+</script>
+
+<div class="box">
+    <h1>{title}</h1>
+    {#if imageURL}
+        <img src={imageURL} alt={imageAlt || "No alt text was provided"} />
+    {/if}
+    <slot></slot>
+</div>
